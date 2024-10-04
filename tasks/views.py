c2a8f37e9700ad4e7cb9df649b9fc1e136e40861
SnayderCJ <Jcedenoj5@unemi.edu.ec>
2024-10-04 13:36:32 -0500

    Views

--- tasks/views.py
+++ tasks/views.py
@@ -12,15 +12,14 @@ def signup(request):
 
     if request.method == 'GET':
         return render(request, 'signup.html', {
-            'form': UserCreationForm()
+            'form': UserCreationForm
         })
     else:
-        if request.POST['password1'] == request.POST['password1']:
+        if request.POST['password1'] == request.POST['password2']:
             try:
                 user = User.objects.create_user(
                     username=request.POST['username'],
-                    password=request.POST['password1']
-                )
+                    password=request.POST['password1'])
                 user.save()
                 return HttpResponse('User created successfully')
             except:
