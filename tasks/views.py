b0bd72b90e0bfce0e2b2ccacd5562c4d836dbe0e
SnayderCJ <Jcedenoj5@unemi.edu.ec>
2024-10-04 18:40:16 -0500

    Views

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.db import IntegrityError
+from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
 from django.http import HttpResponse
 
 # Create your views here.
@@ -9,19 +11,27 @@ def home(request):
     return render(request, 'home.html')
 
 def signup(request):
-
     if request.method == 'GET':
         return render(request, 'signup.html', {
-            'form': UserCreationForm
-        })
+            "form": UserCreationForm
+            })
     else:
-        if request.POST['password1'] == request.POST['password2']:
+
+        if request.POST["password1"] == request.POST["password2"]:
             try:
                 user = User.objects.create_user(
-                    username=request.POST['username'],
-                    password=request.POST['password1'])
+                    request.POST["username"], 
+                    password=request.POST["password1"])
                 user.save()
-                return HttpResponse('User created successfully')
-            except:
-                return HttpResponse('Username already exists')
-        return HttpResponse('Password do not match')
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    "form": UserCreationForm, 
+                    "error": "Username already exists."
+                    })
+
+        return render(request, 'signup.html', {
+            "form": UserCreationForm, 
+            "error": "Passwords did not match."
+            })
