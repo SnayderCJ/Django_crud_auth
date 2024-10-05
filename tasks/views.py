aa701784f7944a98cc8d82d697d0d2e6c58572a5
SnayderCJ <Jcedenoj5@unemi.edu.ec>
2024-10-05 13:40:57 -0500

    Create_Task

--- tasks/views.py
+++ tasks/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
+from .forms import TaskForm
 
 # Create your views here.
 
@@ -37,6 +38,18 @@ def signup(request):
     
 def tasks(request):
     return render(request, 'tasks.html')
+
+def create_task(request):
+
+    if request.method == 'GET':
+        return render(request, 'create_task.html', {
+            'form': TaskForm
+            })
+    else:
+        
+        return render(request, 'create_task.html', {
+            'form': TaskForm
+            })
 
 def signout(request):
     logout(request)
